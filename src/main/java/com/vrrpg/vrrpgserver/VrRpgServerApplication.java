96145a7392e3936efd6bbee0ed997fc86a25d8c2
Gyorgy Bucsek <gyorgy.bucsek@gmail.com>
2017-06-12 10:13:08 +0200

    Create empty Spring project

---
+++ src/main/java/com/vrrpg/vrrpgserver/VrRpgServerApplication.java
@@ -0,0 +1,12 @@
+package com.vrrpg.vrrpgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrRpgServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrRpgServerApplication.class, args);
+    }
+}
