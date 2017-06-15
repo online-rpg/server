7603c1989487583bd298fd3de332a8cf962fb4ba
Gyorgy Bucsek <gyorgy.bucsek@gmail.com>
2017-06-15 09:21:52 +0200

    Save connected clients ID and broadcast it in case of close event

--- src/main/java/com/vrrpg/server/socket/SocketHandler.java
+++ src/main/java/com/vrrpg/server/socket/SocketHandler.java
@@ -11,8 +11,8 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Component
@@ -20,17 +20,20 @@ class SocketHandler extends TextWebSocketHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(SocketHandler.class);
 
     private final ObjectMapper objectMapper;
-    private final Collection<WebSocketSession> aliveSessions;
+    private final Map<WebSocketSession, String> aliveSessions;
 
     SocketHandler(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
-        aliveSessions = new HashSet<>();
+        aliveSessions = new HashMap<>();
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         LOGGER.trace("handleTextMessage - {}, {}", session, message);
         GameMessage gameMessage = objectMapper.readValue(message.getPayload(), GameMessage.class);
+        if ("join".equalsIgnoreCase(gameMessage.getEventName())) {
+            aliveSessions.put(session, gameMessage.getEventSource());
+        }
         broadcast(gameMessage, session);
         super.handleTextMessage(session, message);
     }
@@ -38,19 +41,23 @@ class SocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         LOGGER.trace("afterConnectionEstablished - {}", session);
-        aliveSessions.add(session);
+        aliveSessions.put(session, "");
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         LOGGER.trace("afterConnectionClosed - {}, {}", session, status);
+        if (aliveSessions.containsKey(session)) {
+            String eventSource = aliveSessions.get(session);
+            broadcast(new GameMessage("leave", eventSource, new HashMap<>()), session);
+        }
         aliveSessions.remove(session);
     }
 
     private void broadcast(GameMessage message, WebSocketSession currentSession) {
         try {
             TextMessage textMessage = new TextMessage(objectMapper.writeValueAsString(message));
-            aliveSessions.stream().filter(s -> !Objects.equals(s, currentSession)).forEach(aliveSession -> {
+            aliveSessions.keySet().stream().filter(s -> !Objects.equals(s, currentSession)).forEach(aliveSession -> {
                 try {
                     aliveSession.sendMessage(textMessage);
                 } catch (IOException e) {
