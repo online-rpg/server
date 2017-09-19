9a8e6ae86f28584be84bb95c2deedfe757d1baf3
Gyorgy Bucsek <gyorgy.bucsek@gmail.com>
2017-09-19 10:33:41 +0200

    Create models for game description

---
+++ src/main/java/com/vrrpg/server/mongo/MeshDescriptionRepository.java
@@ -0,0 +1,6 @@
+package com.vrrpg.server.mongo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeshDescriptionRepository extends CrudRepository<MeshDescription, String> {
+}
