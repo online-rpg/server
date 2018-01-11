efc32da89111f87f7fabcd0f0b29fec3e3a8fa2e
György Bucsek <gyorgy.bucsek@gmail.com>
2018-01-11 10:28:49 +0100

    Remove unused ServletException notation in CorsFilter

--- src/main/java/com/vrpg/server/security/CorsFilter.java
+++ src/main/java/com/vrpg/server/security/CorsFilter.java
@@ -32,6 +32,6 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig config) throws ServletException {
+    public void init(FilterConfig config) {
     }
 }
