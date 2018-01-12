4eb5da21536243fff549ae49677a7bc0cef6ab67
György Bucsek <gyorgy.bucsek@gmail.com>
2018-01-12 15:30:57 +0100

    Remove unused imports in UserManagementController

--- src/main/java/com/vrpg/server/api/usermanagement/UserManagementController.java
+++ src/main/java/com/vrpg/server/api/usermanagement/UserManagementController.java
@@ -9,14 +9,12 @@ import com.vrpg.server.api.exception.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Controller
 class UserManagementController {
